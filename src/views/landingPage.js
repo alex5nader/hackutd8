abcf4078a0920a872a0aa0b6b231557576b42b9e
Abby Thomas <69169237+thomasabigail@users.noreply.github.com>
2021-11-13 16:11:43 -0600

    added landing page

---
+++ src/views/landingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/landingPage.css';
+
+function landingPage()
+{
+    return(
+        <div>
+            
+        </div>
+    )
+}
