12fce3b35adcbd190523b44f934a6c9cfff8df69
Abby Thomas <69169237+thomasabigail@users.noreply.github.com>
2021-11-14 07:23:43 -0600

    Removed defaults

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const API_KEY = "e312da15354a7d2e211682e3c535852b";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>Hi, my name is Abby</p>
       </header>
     </div>
